77abeee6a5fb4ad6a8f978af0ed26b19ba8ef31a
Davi Projetos <g4mern4ato@gmail.com>
2024-09-28 00:31:46 -0300

    Adicionar arquivos de projeto.

---
+++ ViewComponents/SaudacaoAlunocsViewComponents.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunocsViewComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //Pegar o aluno lá na base de dados!!! 
+            //Pegar o dado (nome) do aluno quew está logado!
+            var aluno = new Aluno() { Nome = "Eduardo" };
+
+            return View(aluno);
+        }
+    }
+}
